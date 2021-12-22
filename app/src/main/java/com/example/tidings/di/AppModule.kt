bcfeccc196e9fda1126a9706a94f9e0dc03cdc26
Halilkrkn <halilkrkn000@gmail.com>
2021-12-22 20:08:42 +0300

    okHttp3 library added.
    -okHttp3 kütüphanesi kullanılarak Interceptor methodu ile ağ üzerindeki tafiklerin log yani kayıtlarını aldık ve client üzerinde ağ trafiğini kontrol ettirdik..

--- app/src/main/java/com/example/tidings/di/AppModule.kt
+++ app/src/main/java/com/example/tidings/di/AppModule.kt
@@ -6,6 +6,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -15,12 +17,28 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+
+    // HttpLoggingInterceptor ile ağ işlemleri içersindeki tafiğği kayıt etmek için ve
     @Singleton
     @Provides
-    fun provideRetrofit(): Retrofit =
+    fun logging(logging: HttpLoggingInterceptor): HttpLoggingInterceptor {
+        return logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    @Singleton
+    @Provides
+    fun client(logging: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(logging) //Interceptor bir api isteğini takip edebilen, yeniden oluşturulabilen ve tekrardan istek atmasını sağlayan yapılardır.
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(client: OkHttpClient): Retrofit =
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
 
     @Singleton
